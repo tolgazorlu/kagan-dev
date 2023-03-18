1f2310d2e6a148ac47b3a2627255dcb107788881
tolgazorlu <tolgazorlu17@gmail.com>
2023-03-18 12:34:18 +0300

    about hero fixed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,17 +9,17 @@ export default function About() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
-      <div className="p-10 md:p-36 md:h-full w-full flex justify-center items-center flex-col sm:flex-col lg:flex-row gap-2 md:gap-24">
-        <div className="w-96 h-96 rounded-md">
+      <div className="flex items-center justify-start">
+        <div className="w-1/2">
           <img src={Flyme} />
         </div>
-        <div className="flex flex-col gap-4 w-6/12 md:w-3/12 h-80">
+        <div className="flex flex-col gap-4 md:w-3/12 h-80">
           <div className="text-4xl font-bold font-orbitron">
             <span className="text-black">KAGAN</span>{" "}
             <span className="text-cyan-500">AYTEN</span>
           </div>
-          <div className="text-md">
-            <span className="text-black font-orbitron">
+          <div>
+            <span>
               Indie Game Developer
             </span>
           </div>
@@ -27,7 +27,7 @@ export default function About() {
             <hr />
           </div>
           <div>
-            <p className="text-black">
+            <p>
               My name is Kagan Ayten. I am a unity game developer, and I'm very
               passionate and dedicated to my work.
             </p>
